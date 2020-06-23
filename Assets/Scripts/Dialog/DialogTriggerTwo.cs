c1f1192d13d675afee7983b151cbef2d8e453b86
Luckerisback <61817870+Luckerisback@users.noreply.github.com>
2020-06-23 19:12:45 +0700

    Доделана система диалога

---
+++ Assets/Scripts/Dialog/DialogTriggerTwo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogTriggerTwo : MonoBehaviour
+{
+    private bool check;
+    public Dialog dialog;
+    public CrystalsCounter cr;
+   
+    private void Start()
+    {
+        check = false;
+    }
+    private void Update()
+    {
+        if(cr.countForTrigger == 3 && check == false)
+        {
+            TriggerDialog();
+            check = true;
+           
+        }
+    }
+
+    public void TriggerDialog()
+    {
+        FindObjectOfType<DialogManager>().StartDialog(dialog);
+    }
+}

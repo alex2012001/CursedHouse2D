ba1278590cf6a0fe22a0e8e4ae6b0684edb2097d
alex2012001 <alex2012001@users.noreply.github.com>
2020-06-24 14:24:54 +0700

    Добавил музыку и звуки в главном меню и в меню выбора уровня

---
+++ Assets/Scripts/Music and Sounds/MusicPanelOnLevelChoice.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class MusicPanelOnLevelChoice : MonoBehaviour
+{
+    public AudioSource buttonClick;
+
+    public AudioMixerGroup soundMixer;
+    public AudioMixerGroup musicMixer;
+
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("MusicEnabled") == 1)
+        {
+            musicMixer.audioMixer.SetFloat("MusicVolume", 0);
+        }
+        else if (PlayerPrefs.GetInt("MusicEnabled") == 2)
+        {
+            musicMixer.audioMixer.SetFloat("MusicVolume", -80);
+        }
+        else
+        {
+            musicMixer.audioMixer.SetFloat("MusicVolume", 0);
+        }
+
+        if (PlayerPrefs.GetInt("SoundEnabled") == 1)
+        {
+            soundMixer.audioMixer.SetFloat("SoundVolume", 0);
+        }
+        else if (PlayerPrefs.GetInt("MusicEnabled") == 2)
+        {
+            soundMixer.audioMixer.SetFloat("SoundVolume", -80);
+        }
+        else
+        {
+            soundMixer.audioMixer.SetFloat("SoundVolume", 0);
+        }
+    }
+
+    public void ButtonSound()
+    {
+        buttonClick.Play();
+    }
+
+
+}
